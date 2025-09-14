50516d7441225f97f5be9c2c0e5e1fe85b3603c7
Ivan <picea.abies10@gmail.com>
2025-09-14 15:23:41 +0300

    It is possible to launch through nginx.

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,24 +1,21 @@
-// import axios from 'axios';
-// import { handleApiError } from './interceptors';
-//
-// const isProduction = import.meta.env.MODE === 'production';
-// const baseURL = isProduction ? '/api' : 'http://localhost:5000/api';
-//
-// export const api = axios.create({
-//   baseURL,
-// });
-//
-// api.interceptors.response.use(response => response, handleApiError);
-
-// ________________________________________________________
-
 import axios from 'axios';
 import { handleApiError } from './interceptors';
 
-export const api = axios.create({
-  baseURL: '/api',
-});
+const isProduction = import.meta.env.MODE === 'production';
+const baseURL = isProduction ? '/api' : 'http://localhost:5000/api';
 
-export const BASE_URL = '/api';
+export const api = axios.create({
+  baseURL,
+});
+// ________________________________________________________
+
+// import axios from 'axios';
+// import { handleApiError } from './interceptors';
+//
+// export const api = axios.create({
+//   baseURL: '/api',
+// });
+//
+// export const BASE_URL = '/api';
 
 api.interceptors.response.use(response => response, handleApiError);
